c13cb133264461b49fd73d89aa65e40c2deda6f0
ValperDev <jdulcedev@gmail.com>
2024-06-06 21:32:54 -0500

    style: add style to the navbar component, add Roboto font and change images path

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import "./Navbar.css";
-import logo from "../../assets/logo.png";
-import arrow_icon from "../../assets/arrow_icon.png";
+import logo from "../../assets/images/logo.png";
+import arrow_icon from "../../assets/images/arrow_icon.png";
 
 function Navbar() {
   return (
-    <div className='navbar'>
-      <img src={logo} alt="" />
-      <ul>
-        <li>Home</li>
-        <li>Features</li>
-        <li>Pricing</li>
-        <li>Blog</li>
+    <div className="navbar">
+      <img src={logo} alt="" className="navbar__logo"/>
+      <ul className="navbar__menu">
+        <li className="navbar__menu-item">Home</li>
+        <li className="navbar__menu-item">Features</li>
+        <li className="navbar__menu-item">Pricing</li>
+        <li className="navbar__menu-item">Blog</li>
       </ul>
-      <div className="nav-right">
-        <select>
+      <div className="navbar__nav-right">
+        <select className="navbar__select">
           <option value="usd">usd</option>
           <option value="eur">eur</option>
         </select>
-        <button>Sign up <img src={arrow_icon} alt="" /></button>
+        <button className="navbar__button">Sign up <img src={arrow_icon} alt="" className="navbar__button-icon"/></button>
       </div>
     </div>
   )

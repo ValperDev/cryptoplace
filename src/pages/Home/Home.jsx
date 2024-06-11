0a7f9f29ce3b9fd7c1fe64ea2758ce5be566155f
ValperDev <jdulcedev@gmail.com>
2024-06-10 21:28:13 -0500

    feat: add home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,25 @@ import "./Home.css";
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="home">
+      <div className="hero">
+        <h1 className="hero__title">Largest Crypto Marketplace</h1>
+        <p className="hero__description">Welcome to the world&apos;s largest cryptocurrency marketplace. Sign up to explore more about cryptos.</p>
+        <form className="hero__form">
+          <input className="hero__input" type="text" placeholder="Search crypto..."/>
+          <button className="hero__button">Search</button>
+        </form>
+      </div>
+      <div className="crypto-table">
+        <div className="crypto-table__layout">
+          <p className="table-layout__column">#</p>
+          <p className="table-layout__column">Coins</p>
+          <p className="table-layout__column">Price</p>
+          <p className="table-layout__column">24H Change</p>
+          <p className="table-layout__column">Market Cap</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
